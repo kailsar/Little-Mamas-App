22ca73c28078c02be817046d7fc3ec291df79cf7
Christopher <c.g.burnett@gmail.com>
2018-09-27 14:33:05 +0000

    Create database if it doesn't exist

--- LittleMamasApp.py
+++ LittleMamasApp.py
@@ -1,9 +1,29 @@
 import mysql.connector
+from mysql.connector import errorcode
 
 mydb = mysql.connector.connect(
-  host="terraform-20180927105136845700000001.comuflukwjwy.eu-west-1.rds.amazonaws.com",
+  host="terraform-20180927132540783300000007.comuflukwjwy.eu-west-1.rds.amazonaws.com",
   user="admin",
   passwd="9aDrG}GK$>[#E4A"
 )
+cursor = mydb.cursor()
+DB_NAME = "MAMAS_DB"
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
 
-print(mydb)
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exist.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        mydb.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
